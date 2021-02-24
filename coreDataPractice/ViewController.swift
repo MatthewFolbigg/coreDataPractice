2c56868f236ca327a543b0cb0cb59caa25f18722
Matthew Folbigg <matthew.folbigg@icloud.com>
2021-02-24 11:34:53 +0000

    Initial Commit

---
+++ coreDataPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coreDataPractice
+//
+//  Created by Matthew Folbigg on 24/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
